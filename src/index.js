39bc40866643dda92eadc0973e1a02552afc3fd7
chitthet-cyber <fernandoaung28@gmail.com>
2021-01-10 10:05:28 +0630

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider, GithubContext } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+require('dotenv').config();
 
 ReactDOM.render(
 
